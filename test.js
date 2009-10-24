06ad08f5c643299d59ece46ac4988e032252edb3
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-10-24 21:16:04 +0300

    Add functional test for rendering the game board in the browser

--- test.js
+++ test.js
@@ -9,3 +9,12 @@ f.ok(game.elementAt(0, 0) === 1, "Test elementAt();");
 f.compare("X-\n--", render(game), "Test render();");
 
 f.testDone();
+
+window.addEventListener("DOMContentLoaded", function () {
+    var game = new Game([
+        [1, 0, 1],
+        [0, 0, 1],
+        [1, 0, 1],
+    ]);
+    document.getElementById("panel").innerHTML = render(game);
+}, false);
