a506fc43d91176c17f0d1371709f79f244797db8
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-10-24 21:10:50 +0300

    Implement render() function

--- game.js
+++ game.js
@@ -5,3 +5,26 @@ var Game = function (board) {
 Game.prototype.elementAt = function (x, y) {
     return this.board[x][y];
 };
+
+Game.prototype.eachRow = function (callback) {
+    this.board.map(function (row, i, rows) {
+        row.eachCell = row.map;
+        callback(row, i, rows);
+    });
+};
+
+var render = function (game) {
+    var rows = [];
+
+    game.eachRow(function (row, i) {
+        var cells = "";
+
+        row.eachCell(function (cell, i) {
+            cells += (cell === 1) ? "X" : "-";
+        });
+
+        rows.push(cells);
+    });
+
+    return rows.join("\n");
+};
